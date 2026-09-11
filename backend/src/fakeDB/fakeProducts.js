6fd3ae0bc6a02faee836602745ed30dd648e8aa7
NidnipaRin <rarin.nidnipa@gmail.com>
2026-09-11 10:10:37 +0700

    add fakeproduct

---
+++ backend/src/fakeDB/fakeProducts.js
@@ -0,0 +1,28 @@
+let products = [
+  {
+    id: "1",
+    name: "Rocky Dragon Rocking Horse",
+    price: 1590,
+    quantity: 3,
+  },
+  {
+    id: "2",
+    name: "Suction Silicone Baby Bowl with Spoon Set",
+    price: 350,
+    quantity: 10,
+  },
+  {
+    id: "3",
+    name: "Anti-Colic Straw Sippy Cup",
+    price: 490,
+    quantity: 6,
+  },
+  {
+    id: "4",
+    name: "Organic Water Baby Wipes (3-Pack)",
+    price: 199,
+    quantity: 15,
+  },
+];
+
+export { products };

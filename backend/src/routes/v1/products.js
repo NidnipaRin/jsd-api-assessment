5e466666fab2163a0847e8cdd04321d7a4388f99
NidnipaRin <rarin.nidnipa@gmail.com>
2026-09-11 10:40:51 +0700

    add get /:id

--- backend/src/routes/v1/products.js
+++ backend/src/routes/v1/products.js
@@ -3,12 +3,12 @@ import { products } from "../../fakeDB/fakeProducts.js";
 
 export const router = Router();
 
-// Read products (GET /)
+// Read All Products (พร้อม Search Query String)
 router.get("/", (req, res, next) => {
   try {
     const { search } = req.query;
 
-    // ถ้ามีการส่ง ?search= มา ให้ filter ตามชื่อ
+    // ถ้ามีการส่ง ?search= มา ให้ filter กรองตามชื่อที่พิมพ์หามา
     if (search) {
       const filteredProducts = products.filter((p) =>
         p.name.toLowerCase().includes(search.toLowerCase()),
@@ -17,7 +17,22 @@ router.get("/", (req, res, next) => {
     }
 
     // ถ้าไม่มี search ส่งมา ก็คืนค่า products ทั้งหมด
-    res.json(products);
+    return res.status(200).json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Read One Product by ID
+router.get("/:id", (req, res, next) => {
+  try {
+    const product = products.find((p) => p.id === req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    return res.status(200).json(product);
   } catch (err) {
     next(err);
   }
